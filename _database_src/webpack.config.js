916f5d415f1f96a9c69a43e4ac6cabd2b66dadfe
igboyes <igboyes@gmail.com>
2018-01-16 10:37:35 -0800

    Add database js src

---
+++ _database_src/webpack.config.js
@@ -0,0 +1,34 @@
+var path = require("path");
+var HTMLPlugin = require("html-webpack-plugin");
+var CleanPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+
+    entry: ["babel-polyfill", path.resolve(__dirname, "./main.js")],
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: "babel-loader"
+            }
+        ]
+    },
+
+    output: {
+        path: path.resolve(__dirname, "../database"),
+        filename: "app.[hash:8].js"
+    },
+
+    plugins: [
+        new HTMLPlugin({
+            filename: "index.html",
+            template: "index.html"
+        }),
+
+        new CleanPlugin([path.resolve(__dirname, "../database")], {
+            verbose: true
+        })
+    ]
+}
