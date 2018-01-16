6ec1086795f2c5e00249e10d26593c718fb05d2d
igboyes <igboyes@gmail.com>
2018-01-16 11:11:24 -0800

    Add React placeholder component

---
+++ _database_src/main.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Test = () => (
+    <div>
+        This is a Test
+    </div>
+);
+
+ReactDOM.render(<Test />, document.getElementById("app-container"));
