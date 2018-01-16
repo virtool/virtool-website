d21f752b99a7724cf5731682255e936a38d77df5
Unknown <mimiijima@gmail.com>
2018-01-16 14:30:06 -0800

    Initialize client code

--- _database_src/main.js
+++ _database_src/main.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Request from "superagent";
 
-const Test = () => (
-    <div>
-        This is a Test
-    </div>
-);
+import Test from "./component";
+
+Request
+   .get('/releases')
+   .then((res) => {
+       const text = res.text;
+       const releases = JSON.parse(text);
+       const latest = releases.database[0];
+       const url = latest.download_url;
+
+       return Request.get(url);    
+   })
+   .then((res) => {
+       console.log(res);
+   })
 
 ReactDOM.render(<Test />, document.getElementById("app-container"));
