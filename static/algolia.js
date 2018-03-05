801b9aa006e9624c6c2b0c0b21d6e4a2f466d283
naomi <mimiijima@gmail.com>
2018-03-05 08:51:51 -0800

    Add search query render

--- static/algolia.js
+++ static/algolia.js
@@ -1,20 +1,27 @@
+
+/*
 function handleInputChange (e, index) {
     var searchTerm = e.target.value;
 
     index.search(searchTerm, function(err, content) {
         console.log(content.hits);
 
-        var searchResultList = "<div class='list-group' id='search-results'></div>";
+        var searchResultList = "<div class='dropdown is-active'>" +
+            "<div class='dropdown-content' id='search-results'></div>" +
+            "</div>";
 
         $("#manualSearch").parent().append(searchResultList);
 
         content.hits.forEach((hit) => {
-            var searchResult = '<button type="button" class="list-group-item">' + hit.title + '</button>';
+            var searchResult = '<a href="#" class="dropdown-item">' + hit.title + '</a>';
             $("#search-results").append(searchResult);
-            console.log(hit.title);
         });
     });
 
+}
+
+function clearResults () {
+    $("#search-results").remove();
 }
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -24,16 +31,33 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var searchInput = document.getElementById("manualSearch");
 
-    searchInput.addEventListener("change", (e) => {handleInputChange(e, index)});
+//    searchInput.addEventListener("change", (e) => {handleInputChange(e, index)});
 
-    window.onload = function() {
-        if (window.jQuery) {
-            console.log("yes jQuery");
-            var $test = $(".manualSearch");
-            console.log($test);
-        } else {
-            console.log("no jQuery");
-        }
+    $("#manualSearch").keyup((e) => {
+        clearResults();
+        handleInputChange(e, index);
+    });
+
+});
+*/
+
+$(document).ready(function () {
+
+    var client = algoliasearch('E63DIHARJ5', '8711dec9ef7500381bc6fc86aa1fe4ce');
+    var index = client.initIndex('virtool-docs');
+
+    $("#manualSearch").keyup((e) => {
+        search(e.target.value);
+    });
+
+    function search(searchTerm) {
+        return index.search(searchTerm, function (err, content) {
+            renderResults(content.hits, searchTerm);
+        });
     }
 
+    function renderResults (searchResults, searchTerm) {
+        console.log(searchResults);
+    }
+    
 });
