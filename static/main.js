215d86dbb36d45c2741a96ef6c3cb073992af715
igboyes <igboyes@gmail.com>
2018-11-22 13:40:27 -0800

    Drop JQuery and fix JS bugs

--- static/main.js
+++ static/main.js
@@ -1,9 +1,73 @@
+var algoliaClient = algoliasearch(
+  "E63DIHARJ5",
+  "8711dec9ef7500381bc6fc86aa1fe4ce"
+);
+
+var algoliaIndex = algoliaClient.initIndex("virtool-docs");
+
+// Removes previous search and dropdown
+function clearResults() {
+  var $el = document.getElementById("search-results");
+  $el.classList.remove("is-active");
+  $el.innerHtml = "";
+}
+
+// Creates dropdown with search results
+function renderResults(searchResults, searchTerm) {
+  clearResults();
+
+  if (!searchResults.length) {
+    return;
+  }
+
+  var $el = document.getElementById("search-results");
+  $el.classList.add("dropdown-custom-content");
+
+  searchResults.forEach(hit => {
+    var $result = document.createElement("a");
+    $result.href = `/${hit.uri}`;
+    $result.classList.add("dropdown-item-custom");
+    $result.innerHTML = `
+      <span class="hit-title">
+          <strong>${hit.title}</strong>
+      </span>
+    `;
+    $el.append($result);
+  });
+}
+
+function hideOnClickOutside(selector) {
+  document.addEventListener("click", function(e) {
+    if ($(e.target).find(selector).length) {
+      $(selector).hide();
+    } else {
+      $(selector).show();
+    }
+  });
+}
+
+function search(searchTerm) {
+  if (!searchTerm.length) {
+    clearResults();
+    return;
+  }
+
+  return algoliaIndex.search(
+    { query: searchTerm, facetFilters: ["type:manual"] },
+    function(err, content) {
+      renderResults(content.hits, searchTerm);
+    }
+  );
+}
+
 function sideMenu() {
   var menuLists = document.querySelectorAll(".side-sub");
 
   menuLists.forEach(function($el) {
     $el.firstElementChild.addEventListener("click", function(e) {
       e.preventDefault();
+
+      console.log("Click");
 
       menuLists.forEach(function($li) {
         $li.classList.remove("is-active");
@@ -17,11 +81,21 @@ function sideMenu() {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
+  sideMenu();
+
   // Get all "navbar-burger" elements
   var $navbarBurgers = Array.prototype.slice.call(
     document.querySelectorAll(".navbar-burger"),
     0
   );
+
+  var $search = document.getElementById("manualSearch");
+
+  if ($search) {
+    $search.addEventListener("keyup", function(e) {
+      search(e.target.value);
+    });
+  }
 
   // Check if there are any navbar burgers
   if ($navbarBurgers.length > 0) {
@@ -38,6 +112,4 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     });
   }
-
-  sideMenu();
 });
